83b2860630529cdd1a2af1a9d7eaf3178639d7b8
Davron01~ <d.mirabdullayev@mail.ru>
2022-12-25 11:44:59 +0500

    This

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-let span = document.querySelector("span");
-let increment = document.querySelector(".inc");
-let decrement = document.querySelector(".dec");
+let w = document.querySelector(".w");
+let h = document.querySelector(".h");
 
-let count = 0;
+let width = window.screen.availWidth;
+let height = window.screen.availHeight;
 
-increment.addEventListener("click", () => {
-  count++;
-  span.innerText = count < 10 && count >= 0 ? "0" + count : count;
-});
-decrement.addEventListener("click", () => {
-  count--;
-  span.innerText = count < 10 && count >= 0 ? "0" + count : count;
-});
+w.innerText = width;
+h.innerText = height;
+// ======================================
+// ===== this,bind,call,apply ======
+// ======================================
+
+function myName() {
+  console.log("Hello My Name is:", this);
+}
+let car = {
+  name: "Cobalt",
+  year: 2020,
+  sayName: myName,
+  info: function (model) {
+    console.log(`Name is: ${this.name}`);
+    console.log(`Year is: ${this.year}`);
+    console.log(`Color is: ${this.color}`);
+    console.log(`Model is: ${model}`);
+  },
+};
+
+let jentra = {
+  name: "Jentra",
+  year: 2019,
+  color: "gray",
+};
+car.info.bind(jentra,"super")();//bind functiyani chaqirsagina ishlaydi
+car.info.call(jentra,"super");//call functiyani birdan uzi chaqirib yuboradi
+car.info.apply(jentra, ["super"]);//apply ham call ga o`hshaydi, faqat array qabul qiladi
